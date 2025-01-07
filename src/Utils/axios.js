95767ed689014c9fe89ae24904e01fdd51a074cf
Ahmad Hassan <ahmadhassanyaseen@gmail.com>
2025-01-07 19:37:29 +0500

    check

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://xeno-3dfkg42x.b4a.run",
     // baseURL: 'http://localhost:5000',
+    
   headers: {
     "Content-Type": "application/json",
   },
