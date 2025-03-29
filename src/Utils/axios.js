565c1d823c033338fdc84d6ba114583016f5ed36
Ahmad Hassan <ahmadhassanyaseen@gmail.com>
2025-03-29 16:22:37 +0500

    new changes q

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://xeno-pi.vercel.app",
+  baseURL: "https://xeno-pi.vercel.app",
   // baseURL: "https://xeno-3dfkg42x.b4a.run",
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   
   headers: {
     "Content-Type": "application/json",
